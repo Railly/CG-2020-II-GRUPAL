e4c2c53bfec0d211640eed813967cc8141f0f5ed
folkloredeluxe <75094207+folkloredeluxe@users.noreply.github.com>
2020-12-21 19:36:41 -0500

    uploaded

--- Tarea07-Transformaciones/src/Utils.hpp
+++ Tarea07-Transformaciones/src/Utils.hpp
@@ -3,7 +3,7 @@
  
 // #define GLEW_STATIC
 #include <GL/glew.h>
- 
+#include <SOIL2/SOIL2.h>
 #include <string>
 #include <fstream>
 #include <iostream>
@@ -105,6 +105,24 @@ namespace Utils {
         }
  
         return program;
+    }
+
+    unsigned int loadTexture(const char *texImagePath) {
+        GLuint textureRef;
+        textureRef = SOIL_load_OGL_texture(texImagePath, SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
+        if (textureRef == 0) 
+            std::cout << "didnt find texture file " << texImagePath << std::endl;
+        // ----- mipmap/anisotropic section
+        glBindTexture(GL_TEXTURE_2D, textureRef);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        if (glewIsSupported("GL_EXT_texture_filter_anisotropic")) {
+            GLfloat anisoset = 0.0f;
+            glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &anisoset);
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, anisoset);
+        }
+        // ----- end of mipmap/anisotropic section
+        return textureRef;
     }
 }
 #endif
--- Tarea07-Transformaciones/src/shaders/vertShader.glsl
+++ Tarea07-Transformaciones/src/shaders/vertShader.glsl
@@ -1,6 +1,10 @@
-#version 330                                                                  
+#version 430                                                                 
                                                                               
-layout (location = 0) in vec3 pos;								  
+layout (location = 0) in vec3 pos;
+layout (location = 1) in vec2 texCoord;				
+out vec2 tc;		
+
+layout(binding = 0) uniform sampler2D samp;
                                                                               
 uniform mat4 u_mv; 
 uniform float u_time;
@@ -44,6 +48,7 @@ void main() {
     }  else {
         gl_Position = u_mv * vec4(pos, 1.0);
     }
+    tc = texCoord;
 }
 // builds and returns a matrix that performs a rotation around the Z axis
 mat4 buildRotateZ(float rad) {
